cefbb553c0a4459b0de8a5ca497101991ec6ea86
Paul Ly <28ly.paul@gmail.com>
2016-01-29 17:51:30 -0800

    cards match and unmatched works

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 /*card flipping and storing code*/
-var a=null;
-var b=null;
-var c=null;
-var d=null;
+var a = null;
+var b = null;
+var c = null;
+var d = null;
 $(document).ready(function(){
-    $('.back').addClass('notFlipped');
-    $('.back').click(function(){
+    $(".back").addClass("notFlipped");
+    $(".back").click(function(){
         $(this).hide();
-        if(a===null){
-            a=$(this).prev().first().attr('src');
-            console.log('first card clicked',a);
-            c=$(this);
-        } else {
-            b = $(this).prev().first().attr('src');
-            console.log('this is card 2',b);
+        if(a === null){
+            a = $(this).prev().first().attr("src");
+            console.log("first card clicked", a);
+            c = $(this);
+        }
+        else {
+            b = $(this).prev().first().attr("src");
+            console.log("this is card 2", b);
             d = $(this);
             compare(a,b);
-            a=null;
-            b=null;
+            a = null;
+            b = null;
             console.log(a,b);
-            c=null;
-            d=null;
+            c = null;
+            d = null;
             console.log(c,d);
         }
     });
 });
 
 function compare(a,b){
-    if(a==b){
-        console.log('match');
-        $(c).removeClass('notFlipped');
-        $(d).removeClass('notFlipped');
-    }else{
-        console.log('no match');
+    if (a==b) {
+        console.log("match");
+        $(c).removeClass("notFlipped");
+        $(d).removeClass("notFlipped");
+    }
+    else {
+        console.log("no match");
         $('.notFlipped').show();
     }
 }
